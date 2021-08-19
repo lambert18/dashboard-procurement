7820391fbb7aa9ffd651e3ab9f079fa9de82b616
Paulinus Lambert Subiantoro <lambertpaulinus@gmail.com>
2021-08-20 02:00:15 +0700

    Moving CSS scripts to CSS files

--- Dash_Website/apps/HOME.py
+++ Dash_Website/apps/HOME.py
@@ -1,16 +1,9 @@
 from os import link
-from dash_bootstrap_components._components.Nav import Nav
 from dash_bootstrap_components._components.NavLink import NavLink
 import dash_html_components as html
 import dash_core_components as dcc
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
-import plotly.express as px
-import pandas as pd
-import pathlib
 from app import app
 
-from apps import HOME, PROC, RMPM, CLEARANCE, INMKT, RMPMSS, NONMKT
 
 layout = html.Div(children=[
     html.H1(children='WELCOME TO PROCUREMENT'),
